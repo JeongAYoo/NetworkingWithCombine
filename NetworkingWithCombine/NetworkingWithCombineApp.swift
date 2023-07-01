8730da91c8575075f5df5d14f85e3fbe370e1f43
Jade Yoo <jeonga038@gmail.com>
2023-07-01 12:09:25 +0900

    Initial Commit

---
+++ NetworkingWithCombine/NetworkingWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingWithCombineApp.swift
+//  NetworkingWithCombine
+//
+//  Created by Jade Yoo on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
